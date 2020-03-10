ec457792a18fb3a2fa5ca9d2c6cd9c1ec1c2bed2
denisgat <denisgatotho@gmail.com>
2020-03-10 14:25:53 -0400

    changes

--- JS/main.js
+++ JS/main.js
@@ -26,7 +26,7 @@ function build(elementtype, classname, id, style, htmlcontent){
 
 //Title
 
-    h1 = build('h1','text-center font-weight-bold','','','TIC TAC TOE')
+    h1 = build('h1','display-3 text-center font-weight-bold','','','TIC TAC TOE')
     titlecol = build('div','col-12','','','','')
     titlerow = build('div','row','','','','')
     titlecontainer = build('container','','','','')
@@ -44,23 +44,33 @@ main = build('main','container mx-auto','','','')
 init.appendChild(main)
 
 // player box
-// var pboxcontainer= build('div','container','','','');
-// var pboxrow = build('div','row','','','')
-// var pboxcol = build('div','col-6 mx-auto','','','')
-// var ptext = build('button','btn btn-lg btn-primary btn-block','btn1','','PLAYER 1:')
+var pboxcontainer= build('div','container','','','');
+var pboxrow = build('div','row m-3','','','')
+var pboxcol = build('div','col-6 mx-auto text-center','','','')
+var ptext = build('h3','','','',"PLAYER: X's TURN")
 
-// footercol.appendChild(resetbtn)
-// footerrow.appendChild(footercol)
-// footercontainer.appendChild(footerrow)
-// footer.appendChild(footercontainer)
-// init.appendChild(footer)
+pboxcol.appendChild(ptext)
+pboxrow.appendChild(pboxcol)
+pboxcontainer.appendChild(pboxrow)
+main.appendChild(pboxcontainer)
 
 //game
 
 let tilenumber = 0;
 let gamerow = "";
-let gamecall = "";
 let state = 0;
+let win = [
+    ["1","2","3"],
+    ["4","5","6"],
+    ["7","8","9"],
+    ["7","5","3"],
+    ["9","5","1"],
+    ["1","4","7"],
+    ["2","5","8"],
+    ["3","6","9"]
+]
+let xarr = [];
+let oarr = [];
 
 gamecontainer = build('div','container mx-auto p-0','','','')
 
@@ -70,7 +80,6 @@ for(let i=1; i <= 3; i++){
             tilenumber = tilenumber + 1
             gamecol = build('div', 'col-4 border-left border-right text-center display-4 pt-4',tilenumber,'','')
             gamecol.onclick = run;
-
             gamerow.appendChild(gamecol)
     }
         gamecontainer.appendChild(gamerow)
@@ -83,9 +92,32 @@ function run(){
     if(this.innerHTML == ""){
         if(state % 2 == 0){
             this.innerHTML = "X"
+            xarr.push(this.id)
+            ptext.innerHTML = "PLAYER: O's TURN"
+
+            for(i=0; i < win.length;i++){
+                if(xarr.includes(win[i][0]) && 
+                xarr.includes(win[i][1]) && 
+                xarr.includes(win[i][2])){
+                ptext.innerHTML = "PLAYER X WINS!!!"
+                ptext.className = "display-4"
+                }
+                
+            }
         }
         else{
             this.innerHTML = "O"
+            oarr.push(this.id)
+            ptext.innerHTML = "PLAYER: X's TURN"
+            for(i=0; i < win.length;i++){
+                if(oarr.includes(win[i][0]) && 
+                oarr.includes(win[i][1]) && 
+                oarr.includes(win[i][2])){
+
+                ptext.innerHTML = "PLAYER O WINS!!!"
+                ptext.className = "display-4"
+                }
+            }
         }
         state += 1
     }
@@ -113,9 +145,10 @@ resetbtn.onclick = function clear(){
     footer.innerHTML = '';
     create();
 }
+console.log(state)
 }
 
-
+// footer.style.display = "none"
 create();
 
 //GAME THEORY 
